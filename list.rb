a0114f0a3333100511820735f85c7e087e64930f
Naoto TAKAHASHI tnaoto@gmail.com <tnaoto@gmail.com>
2014-06-11 01:21:02 +0900

    network

--- list.rb
+++ list.rb
@@ -7,10 +7,34 @@ compute_client = Fog::Compute.new(:provider => :openstack,
                                   :openstack_auth_url => ENV['OS_AUTH_URL'] + "/tokens/"  ,
                                   :openstack_tenant   => ENV['OS_TENANT_NAME'])
 
-p tmp = compute_client.list_flavors
+#p tmps = compute_client.servers.all
 
-net = Fog::Network.new(:provider => :openstack,
+#tmps.each do |tmp|
+#  p server = tmp.name
+#end
+
+servers = compute_client.servers
+
+servers.each do |server|
+  p server.addresses
+  p server.all_addresses
+  p server.ip_addresses
+  p server.floating_ip_addresses
+  p server.public_ip_addresses
+  p server.floating_ip_address
+  p server.public_ip_address
+  p server.private_ip_addresses
+  p server.private_ip_address
+
+end
+
+=begin
+net_cli = Fog::Network.new(:provider => :openstack,
                        :openstack_api_key  => ENV['OS_PASSWORD']  ,
                        :openstack_username => ENV['OS_USERNAME']  ,
                        :openstack_auth_url => ENV['OS_AUTH_URL'] + "/tokens/")
-p net.list_routers
+#p net_cli.methods
+=end
+
+#p net_cli.networks.all
+
