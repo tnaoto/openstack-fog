d04ad2b16122fa743d46957a3b8f36de429b40ee
Naoto TAKAHASHI <tnaoto@gmail.com>
2014-06-29 22:26:33 +0900

    add floatingip

--- floatingip.rb
+++ floatingip.rb
@@ -7,11 +7,21 @@ compute_client = Fog::Compute.new(:provider => :openstack,
                                   :openstack_auth_url => ENV['OS_AUTH_URL'] + "/tokens/"  ,
                                   :openstack_tenant   => ENV['OS_TENANT_NAME'])
 
-
 net_cli = Fog::Network.new(:provider => :openstack,
                        :openstack_api_key  => ENV['OS_PASSWORD']  ,
                        :openstack_username => ENV['OS_USERNAME']  ,
                        :openstack_auth_url => ENV['OS_AUTH_URL'] + "/tokens/")
-p netids = net_cli.list_networks.body['networks'].map{|v| v['id']}
+netids = net_cli.list_networks.body['networks'].map{|v| v['id']}
 
-p compute_client.service
+service = compute_client
+flavor = service.flavors.first
+image = service.images.first
+#server = service.servers.create(:name => 'fog-doc', :flavor_ref => flavor.id,
+#:image_ref => image.id,:nics => ["net_id" => netids[0]])
+
+#net_cli.list_floating_ips.body['floatingips'].each do |aa|
+#  p aa
+#end
+
+p fips = net_cli.list_floating_ips.body['floatingips'].map {|_| _['floating_ip_address']}
+#p service.associate_address(server.id,fips[0])
